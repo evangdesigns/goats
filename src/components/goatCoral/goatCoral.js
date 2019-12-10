f8bacdc4e6bb84a959c3a04ac88866e6e7a9d599
Evan Grabenstein <evangdesigns@gmail.com>
2019-12-09 20:19:21 -0600

    redered goat cards

--- src/components/goatCoral/goatCoral.js
+++ src/components/goatCoral/goatCoral.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Goat from '../goat/goat';
 
 class GoatCoral extends React.Component {
   render() {
     const myGoats = this.props.goats;
 
+    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat} />);
+
     return (
       <div className="goatCoral">
-        Goat Coral
+        {goatCards}
       </div>
     );
   }
